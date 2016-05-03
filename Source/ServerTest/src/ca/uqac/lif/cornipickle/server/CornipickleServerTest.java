9a43f669be5dfdbad3cc18bef1ff0b95de14aa18
Paul Lesur <paul.lesur@free.fr>
2016-05-02 22:30:10 -0400

    Test class CornipickleServerTest created.

---
+++ Source/ServerTest/src/ca/uqac/lif/cornipickle/server/CornipickleServerTest.java
@@ -0,0 +1,68 @@
+package ca.uqac.lif.cornipickle.server;
+
+import ca.uqac.lif.cornipickle.Interpreter;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by paul on 02/05/16.
+ */
+public class CornipickleServerTest {
+
+    @Test
+    public void estCornipickleServerConstructor(){
+
+        CornipickleServer cs = new CornipickleServer("server", 1234);
+
+        String serverName = cs.getServerName();
+        int serverPort = cs.getServerPort();
+
+
+        assertTrue(serverPort==1234&&"server".equals(serverName));
+    }
+
+    @Test
+    public void testCornipickleServerSetInterpreter(){
+
+        CornipickleServer cs = new CornipickleServer("server", 1234);
+
+        Interpreter i = new Interpreter();
+
+        cs.setInterpreter(i);
+
+        assertTrue(cs.m_interpreter==i);
+
+    }
+    
+    @Test
+    public void testCornipickleServerLastProbeContact(){
+
+        CornipickleServer cs = new CornipickleServer("server", 1234);
+
+        cs.setLastProbeContact();
+        Date lastProbeContact = cs.m_lastProbeContact;
+
+        assertTrue(lastProbeContact==cs.getLastProbeContact());
+
+    }
+    
+    @Test
+    public void testCornipickleEspaceQuotes(){
+
+        String result = CornipickleServer.escapeQuotes("\"word\"");
+        String expected = "\"word\"".replaceAll("\"", "\\\\\"");
+
+        assertTrue(expected.equals(result));
+
+    }
+
+    @Test
+    public void testCornipickleServerReadProperties(){
+        //TODO
+    }
+
+
+}
