e032b72f2225e0457fb214b93896062ceca815b8
Paul Lesur <paul.lesur@free.fr>
2016-05-16 11:41:08 -0400

    Tests on Interpreter

--- Source/ServerTest/src/ca/uqac/lif/cornipickle/InterpreterTest.java
+++ Source/ServerTest/src/ca/uqac/lif/cornipickle/InterpreterTest.java
@@ -5,6 +5,8 @@ package ca.uqac.lif.cornipickle;/**
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class InterpreterTest {
@@ -35,13 +37,42 @@ public class InterpreterTest {
         assertTrue(i.getPredicates().toString().contains(pd.toString()));
     }
 
-    @Test
+    /*@Test
     public void InterpreterTestGetProperty() throws CornipickleParser.ParseException {
-        //ElementPropertyComplement epc = (ElementPropertyComplement)UtilsTest.shouldParseAndNotNullReturnElement(i.m_parser, "", "<elem_property>");
         i.parseProperties("Eventually ( \"3\" equals \"3\").\nEventually ( \"3\" equals \"3\").\nEventually ( \"3\" equals \"3\").");
 
         //System.out.println(i.m_statements);
+    }*/
+    
+    @Test
+    public void InterpreterTestGetSetDefinition(){
+
+
+        SetDefinition sd = new SetDefinition(new StringConstant("def1"));
+        SetDefinition sd2 = new SetDefinition(new StringConstant("def2"));
+
+        i.m_setDefs.put("def1", sd);
+        i.m_setDefs.put("def2", sd2);
+
+        List<SetDefinition> list = i.getSetDefinitions();
+
+        boolean b1 = list.contains(sd);
+        boolean b2 = list.contains(sd2);
+
+        assertTrue(b1&&b2);
     }
+
+
+    /*@Test
+    public void InterpreterTestGetAttributes() throws CornipickleParser.ParseException {
+        //ElementPropertyComplement epc = (ElementPropertyComplement)UtilsTest.shouldParseAndNotNullReturnElement(i.m_parser, "", "<elem_property>");
+        i.parseProperties("Eventually ( \"3\" equals \"3\").\nEventually ( \"3\" equals \"3\").\nEventually ( \"3\" equals \"3\").");
+
+
+        System.out.println(i.m_statements);
+    }*/
+
+
 
 
 }
