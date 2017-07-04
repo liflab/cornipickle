c83ae2b02038ecb0ecfa5cb4e64963345d27048e
Faterou <francis_tofu@hotmail.com>
2017-07-04 14:41:51 -0400

    Made a test that doesn't pass for some reason... help.

--- Source/ServerTest/src/ca/uqac/lif/cornipickle/WhenIsNowTest.java
+++ Source/ServerTest/src/ca/uqac/lif/cornipickle/WhenIsNowTest.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import ca.uqac.lif.bullwinkle.BnfParser;
 import ca.uqac.lif.bullwinkle.ParseNode;
+import ca.uqac.lif.cornipickle.CornipickleParser.ParseException;
 import ca.uqac.lif.cornipickle.util.PackageFileReader;
 import ca.uqac.lif.json.*;
 
@@ -13,225 +14,262 @@ import static org.junit.Assert.*;
 
 public class WhenIsNowTest {
 
-    CornipickleParser parser;
-    WhenIsNow win;
-    JsonParser j_parser;
-    JsonElement jse;
+  CornipickleParser parser;
+  WhenIsNow win;
+  JsonParser j_parser;
+  JsonElement jse;
 
-    @Before
-    public void setUp() throws JsonParser.JsonParseException {
+  @Before
+  public void setUp() throws JsonParser.JsonParseException {
 
-        parser = new CornipickleParser();
+    parser = new CornipickleParser();
 
-        String line = "When $d is now $y ( $d's width equals (200 + 100) )\n";
+    String line = "When $d is now $y ( $d's width equals (200 + 100) )\n";
 
-        ParseNode pn = shouldParseAndNotNull(line, "<when>");
-        LanguageElement e = parser.parseStatement(pn);
+    ParseNode pn = shouldParseAndNotNull(line, "<when>");
+    LanguageElement e = parser.parseStatement(pn);
 
-        if (e == null)
-        {
-            fail("Parsing returned null");
-        }
-        if (!(e instanceof WhenIsNow))
-        {
-            fail("Got wrong type of object");
-        }
-
-        win= (WhenIsNow) e;
-
-
-        j_parser = new JsonParser();
-        String json = PackageFileReader.readPackageFile(this.getClass(), "data/sample-8.json");
-        jse = j_parser.parse(json);
+    if (e == null)
+    {
+      fail("Parsing returned null");
+    }
+    if (!(e instanceof WhenIsNow))
+    {
+      fail("Got wrong type of object");
     }
 
-    @Test
-    public void WhenIsNowTestDefaultConstructor(){
-        WhenIsNow win2 = new WhenIsNow();
-        assertTrue(win2 instanceof Statement);
+    win= (WhenIsNow) e;
+
+
+    j_parser = new JsonParser();
+    String json = PackageFileReader.readPackageFile(this.getClass(), "data/sample-8.json");
+    jse = j_parser.parse(json);
+  }
+
+  @Test
+  public void WhenIsNowTestDefaultConstructor(){
+    WhenIsNow win2 = new WhenIsNow();
+    assertTrue(win2 instanceof Statement);
+  }
+
+
+  @Test
+  public void WhenIsNowTestToString(){
+    String expected = "When $d is now $y (\n$d's width equals 200.0 + 100.0)";
+    assertTrue(expected.equals(win.toString()));
+  }
+  
+  @Test
+  public void WhenIsNowTestIsTemporal(){
+    assertFalse(win.isTemporal());
+  }
+
+  @Test
+  public void WhenIsNowTestGetClone(){
+    WhenIsNow win2 = (WhenIsNow) win.getClone();
+    boolean sameInnerStatement = win.m_innerStatement.toString().equals(win2.m_innerStatement.toString());
+    boolean sameBefore = win.m_variableBefore.equals(win2.m_variableBefore);
+    boolean sameAfter =win.m_variableAfter.equals(win2.m_variableAfter);
+    assertTrue(sameInnerStatement&&sameAfter&&sameBefore);
+  }
+
+  @Test
+  public void WhenIsNowTestResetHistory(){
+    win.resetHistory();
+    assertTrue(win.m_elementNow==null&&win.m_verdict.getValue()== Verdict.Value.INCONCLUSIVE);
+  }
+
+
+  @Test
+  public void WhenIsNowTestFetchWithIdOK() throws JsonParser.JsonParseException {
+    // Create "document"
+    JsonList el = new JsonList();
+    {
+      JsonMap x = new JsonMap();
+      x.put("width", new JsonNumber(100));
+      x.put("tagname", new JsonString("p"));
+      x.put("id", new JsonNumber(0));
+      el.add(x);
     }
-
-
-    @Test
-    public void WhenIsNowTestToString(){
-        String expected = "When $d is now $y (\n$d's width equals 200.0 + 100.0)";
-        assertTrue(expected.equals(win.toString()));
+    {
+      JsonMap x = new JsonMap();
+      x.put("width", new JsonNumber(101));
+      x.put("tagname", new JsonString("q"));
+      x.put("id", new JsonNumber(1));
+      el.add(x);
     }
-    
-    @Test
-    public void WhenIsNowTestIsTemporal(){
-        assertFalse(win.isTemporal());
+    JsonMap main = new JsonMap();
+    main.put("children", el);
+    main.put("tagname", new JsonString("h1"));
+    main.put("cornipickleid", new JsonNumber(1));
+
+
+
+    JsonElement e =WhenIsNow.fetchWithId(main, 1);
+
+    assertTrue(e.toString().equals(main.toString()));
+  }
+
+
+  @Test
+  public void WhenIsNowTestFetchWithIdNonTrouve() throws JsonParser.JsonParseException {
+    // Create "document"
+    JsonList el = new JsonList();
+    {
+      JsonMap x = new JsonMap();
+      x.put("width", new JsonNumber(100));
+      x.put("tagname", new JsonString("p"));
+      x.put("id", new JsonNumber(0));
+      el.add(x);
     }
-
-    @Test
-    public void WhenIsNowTestGetClone(){
-        WhenIsNow win2 = (WhenIsNow) win.getClone();
-        boolean sameInnerStatement = win.m_innerStatement.toString().equals(win2.m_innerStatement.toString());
-        boolean sameBefore = win.m_variableBefore.equals(win2.m_variableBefore);
-        boolean sameAfter =win.m_variableAfter.equals(win2.m_variableAfter);
-        assertTrue(sameInnerStatement&&sameAfter&&sameBefore);
+    {
+      JsonMap x = new JsonMap();
+      x.put("width", new JsonNumber(101));
+      x.put("tagname", new JsonString("q"));
+      x.put("id", new JsonNumber(1));
+      el.add(x);
     }
+    JsonMap main = new JsonMap();
+    main.put("children", el);
+    main.put("tagname", new JsonString("h1"));
+    //main.put("cornipickleid", new JsonNumber(1));
 
-    @Test
-    public void WhenIsNowTestResetHistory(){
-        win.resetHistory();
-        assertTrue(win.m_elementNow==null&&win.m_verdict.getValue()== Verdict.Value.INCONCLUSIVE);
+
+
+    JsonElement e =WhenIsNow.fetchWithId(main, 1);
+
+    assertTrue(e==null);
+
+  }
+
+
+  @Test
+  public void WhenIsNowTestFetchWithIdNonMap(){
+    JsonNumber jn = new JsonNumber(1);
+    assertTrue(WhenIsNow.fetchWithId(jn, 1)==null);
+  }
+
+
+  @Test
+  public void WhenIsNowTestFetchWithIdChildren(){
+    // Create "document"
+    JsonList el = new JsonList();
+    {
+      JsonMap x = new JsonMap();
+      x.put("width", new JsonNumber(100));
+      x.put("tagname", new JsonString("p"));
+      x.put("id", new JsonNumber(0));
+      x.put("cornipickleid", new JsonNumber(1));
+      el.add(x);
     }
-
-
-    @Test
-    public void WhenIsNowTestFetchWithIdOK() throws JsonParser.JsonParseException {
-        // Create "document"
-        JsonList el = new JsonList();
-        {
-            JsonMap x = new JsonMap();
-            x.put("width", new JsonNumber(100));
-            x.put("tagname", new JsonString("p"));
-            x.put("id", new JsonNumber(0));
-            el.add(x);
-        }
-        {
-            JsonMap x = new JsonMap();
-            x.put("width", new JsonNumber(101));
-            x.put("tagname", new JsonString("q"));
-            x.put("id", new JsonNumber(1));
-            el.add(x);
-        }
-        JsonMap main = new JsonMap();
-        main.put("children", el);
-        main.put("tagname", new JsonString("h1"));
-        main.put("cornipickleid", new JsonNumber(1));
-
-
-
-        JsonElement e =WhenIsNow.fetchWithId(main, 1);
-
-        assertTrue(e.toString().equals(main.toString()));
+    {
+      JsonMap x = new JsonMap();
+      x.put("width", new JsonNumber(101));
+      x.put("tagname", new JsonString("q"));
+      x.put("id", new JsonNumber(1));
+      el.add(x);
     }
+    JsonMap main = new JsonMap();
+    main.put("children", el);
+    main.put("tagname", new JsonString("h1"));
 
+    JsonElement e =WhenIsNow.fetchWithId(main, 1);        
 
-    @Test
-    public void WhenIsNowTestFetchWithIdNonTrouve() throws JsonParser.JsonParseException {
-        // Create "document"
-        JsonList el = new JsonList();
-        {
-            JsonMap x = new JsonMap();
-            x.put("width", new JsonNumber(100));
-            x.put("tagname", new JsonString("p"));
-            x.put("id", new JsonNumber(0));
-            el.add(x);
-        }
-        {
-            JsonMap x = new JsonMap();
-            x.put("width", new JsonNumber(101));
-            x.put("tagname", new JsonString("q"));
-            x.put("id", new JsonNumber(1));
-            el.add(x);
-        }
-        JsonMap main = new JsonMap();
-        main.put("children", el);
-        main.put("tagname", new JsonString("h1"));
-        //main.put("cornipickleid", new JsonNumber(1));
+    boolean v1 = e.toString().contains("\"tagname\":\"p\"");
+    boolean v2 = e.toString().contains("\"cornipickleid\":1");
+    boolean v3 = e.toString().contains("\"width\":100");
+    boolean v4 = e.toString().contains("\"id\":0");
 
+    boolean f1 = !e.toString().contains("\"tagname\":\"q\"");
 
+    assertTrue(v1&&v2&&v3&&v4&&f1);
 
-        JsonElement e =WhenIsNow.fetchWithId(main, 1);
-
-        assertTrue(e==null);
-
-    }
-
-
-    @Test
-    public void WhenIsNowTestFetchWithIdNonMap(){
-        JsonNumber jn = new JsonNumber(1);
-        assertTrue(WhenIsNow.fetchWithId(jn, 1)==null);
-    }
-
-
-    @Test
-    public void WhenIsNowTestFetchWithIdChildren(){
-        // Create "document"
-        JsonList el = new JsonList();
-        {
-            JsonMap x = new JsonMap();
-            x.put("width", new JsonNumber(100));
-            x.put("tagname", new JsonString("p"));
-            x.put("id", new JsonNumber(0));
-            x.put("cornipickleid", new JsonNumber(1));
-            el.add(x);
-        }
-        {
-            JsonMap x = new JsonMap();
-            x.put("width", new JsonNumber(101));
-            x.put("tagname", new JsonString("q"));
-            x.put("id", new JsonNumber(1));
-            el.add(x);
-        }
-        JsonMap main = new JsonMap();
-        main.put("children", el);
-        main.put("tagname", new JsonString("h1"));
-
-        JsonElement e =WhenIsNow.fetchWithId(main, 1);        
-
-        boolean v1 = e.toString().contains("\"tagname\":\"p\"");
-        boolean v2 = e.toString().contains("\"cornipickleid\":1");
-        boolean v3 = e.toString().contains("\"width\":100");
-        boolean v4 = e.toString().contains("\"id\":0");
-
-        boolean f1 = !e.toString().contains("\"tagname\":\"q\"");
-
-        assertTrue(v1&&v2&&v3&&v4&&f1);
-
-    }
-    
-    @Test
-	public void testEvaluateAtemporalJsonElementMapOfStringJsonElement() {
+  }
+  
+  @Test
+  public void testEvaluateAtemporalJsonElementMapOfStringJsonElement() {
 		JsonElement je= new JsonNumber(15);	
 		Map<String,JsonElement> test = new HashMap<String, JsonElement>();		
 		win.m_elementNow=je;
 		assertTrue(win.evaluateAtemporal(je, test).m_value.equals(Verdict.Value.FALSE));		
-	}
-    @Test
-	public void testEvaluateTemporalJsonElementMapOfStringJsonElement() {
+  }
+  @Test
+  public void testEvaluateTemporalJsonElementMapOfStringJsonElement() {
 		JsonElement je= new JsonNumber(15);	
 		Map<String,JsonElement> test = new HashMap<String, JsonElement>();		
 		win.m_elementNow=je;
 		assertTrue(win.evaluateTemporal(je, test).m_value.equals(Verdict.Value.FALSE));		
-	}
+  }
+  
+  @Test
+  public void testEvaluateTemporal()
+  {
+    try {
+      win = (WhenIsNow)parser.parseStatement("When $x is now $y ( If ($x's width equals 200) Then ( $y's width equals 300 ) )");
+    } catch (ParseException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
     
-    public ParseNode shouldParseAndNotNull(String line, String start_symbol)
-    {
-        BnfParser p = parser.getParser();
-        //p.setDebugMode(true);
-        p.setStartRule(start_symbol);
-        ParseNode pn = null;
+    JsonMap snapshot1 = new JsonMap();
+    snapshot1.put("tagname", "body");
+    snapshot1.put("cornipickleid", "0");
+    snapshot1.put("width", "200");
+    
+    Map<String, JsonElement> newd = new HashMap<String,JsonElement>();
+    newd.put("$x", snapshot1);
+    
+    win.evaluateTemporal(snapshot1, newd);
+    
+    assertEquals(win.m_verdict.m_value, Verdict.Value.INCONCLUSIVE);
+    
+    JsonMap snapshot2 = new JsonMap();
+    snapshot1.put("tagname", "body");
+    snapshot1.put("cornipickleid", "0");
+    snapshot1.put("width", "250");
+    
+    Map<String, JsonElement> newd2 = new HashMap<String,JsonElement>();
+    newd2.put("$x", snapshot2);
+    
+    win.evaluateTemporal(snapshot2, newd2);
+    
+    assertEquals(win.m_verdict.m_value, Verdict.Value.FALSE);
+  }
+  
+  public ParseNode shouldParseAndNotNull(String line, String start_symbol)
+  {
+    BnfParser p = parser.getParser();
+    //p.setDebugMode(true);
+    p.setStartRule(start_symbol);
+    ParseNode pn = null;
 
-        try
-        {
-            pn = p.parse(line);
-        } catch (BnfParser.ParseException e)
-        {
-            fail(e.toString());
-        }
-        if (pn == null)
-        {
-            fail("Failed parsing expression through grammar: returned null");
-        }
-        return pn;
+    try
+    {
+      pn = p.parse(line);
+    } catch (BnfParser.ParseException e)
+    {
+      fail(e.toString());
     }
-    @Test 
-    public void testprefixAccept(){// a modifier si possible
-    	LanguageElementVisitor test =new AttributeExtractor();
-    	win.prefixAccept(test);
-    	assertTrue(true);
+    if (pn == null)
+    {
+      fail("Failed parsing expression through grammar: returned null");
     }
-    @Test 
-    public void testpostfixAccept(){// a modifier si possible
-    	LanguageElementVisitor test =new AttributeExtractor();
-    	win.postfixAccept(test);
-    	assertTrue(true);
-    }
+    return pn;
+  }
+  
+  @Test 
+  public void testprefixAccept(){// a modifier si possible
+  	LanguageElementVisitor test =new AttributeExtractor();
+  	win.prefixAccept(test);
+  	assertTrue(true);
+  }
+  
+  @Test 
+  public void testpostfixAccept(){// a modifier si possible
+  	LanguageElementVisitor test =new AttributeExtractor();
+  	win.postfixAccept(test);
+  	assertTrue(true);
+  }
 
 
 }
