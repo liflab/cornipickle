96f4fee6e671abd9d44236a8cc838feafb3b0362
Faterou <francis_tofu@hotmail.com>
2017-11-10 17:10:16 -0500

    Created transformation classes

--- Source/Server/src/ca/uqac/lif/cornipickle/transformations/CorniTransformation.java
+++ Source/Server/src/ca/uqac/lif/cornipickle/transformations/CorniTransformation.java
@@ -3,6 +3,7 @@ package ca.uqac.lif.cornipickle.transformations;
 import ca.uqac.lif.cornipickle.faultfinder.Transformation;
 import ca.uqac.lif.json.JsonElement;
 
-public abstract class CorniTransformation implements Transformation<JsonElement> {
+public abstract class CorniTransformation implements Transformation<JsonElement>
+{
 
 }
